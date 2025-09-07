d5d298ffc1c402e675fdc0e48563e68c8eecb20c
Yurei <69757457+Yurei21@users.noreply.github.com>
2025-09-07 21:39:55 +0800

    backend simple stuff

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,1 +1,21 @@
 use actix_web::get{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_cors::Cors;
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().json(serde_json::json!({
+        "message" : "Hello From Actix"
+    }))
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .wrap(Cors::permissive())
+            .service(hello)  
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
+}
