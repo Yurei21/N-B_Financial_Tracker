1e8d62b366db562f48c621a1ab3c45ca6d4e02c5
Yurei <69757457+Yurei21@users.noreply.github.com>
2025-09-08 05:42:11 +0800

    backend changes

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,18 +1,22 @@
+mod db;
+
 use actix_web::get{get, post, web, App, HttpResponse, HttpServer, Responder};
 use actix_cors::Cors;
+use sqlx::SqlitePool;
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().json(serde_json::json!({
-        "message" : "Hello From Actix"
-    }))
+async fn health() -> impl Responder {
+    HttpResponse::Ok().body("OK")
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    let pool = db::get_pool().await;
+
+    HttpServer::new(move || {
         App::new()
+            .app_data(web::Data::new(pool.clone()))
             .wrap(Cors::permissive())
+            .route("/health", web::get().to(health))
             .service(hello)  
     })
     .bind("127.0.0.1:8080")?
