a4627fb18475ee9e028d0756869cf29b94da5ee6
Yurei <69757457+Yurei21@users.noreply.github.com>
2025-09-19 21:59:35 +0800

    connection for db in main

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,25 +1,15 @@
 mod db;
+mod config;
 
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 use actix_cors::Cors;
-use sqlx::SqlitePool;
-
-async fn health() -> impl Responder {
-    HttpResponse::Ok().body("OK")
-}
+use config::Config;
+use db::connect;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let pool = db::get_pool().await;
-
-    HttpServer::new(move || {
-        App::new()
-            .app_data(web::Data::new(pool.clone()))
-            .wrap(Cors::permissive())
-            .route("/health", web::get().to(health))
-            .service(hello)  
-    })
-    .bind("127.0.0.1:8080")?
-    .run()
-    .await
+    let config = Config::from_env();
+    let db = connect(&config).await;
+    println!("Connected to the database");
+    Ok(())
 }
