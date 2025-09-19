3568d2c89c01f3d1ded3afdff1d785f09cb6adbb
Yurei <69757457+Yurei21@users.noreply.github.com>
2025-09-19 21:59:26 +0800

    added db.rs

---
+++ backend/src/db.rs
@@ -0,0 +1,8 @@
+use sea_orm::{Database, DatabaseConnection};
+use crate::config::Config;
+
+pub async fn connect(config: &Config) -> DatabaseConnection {
+    Database::connect(&config.database_url)
+        .await
+        .expect("Failed to connect to database.");
+}
